861f5efeeb6ffac17992c8cc7af69501e80882c7
Kayque de Freitas <kayquefreitas08@gmail.com>
2024-03-15 12:58:27 -0300

    commit inicial

---
+++ src/main/java/exaula08/kayquedefreitas/Figura.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2024 Kayque de Freitas <kayquefreitas08@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package exaula08.kayquedefreitas;
+
+/**
+ *
+ * @author Kayque de Freitas <kayquefreitas08@gmail.com>
+ * @data 15/03/2024
+ * @brief Class Figura
+ */
+public abstract class Figura {
+    private String cor;
+    
+    public Figura (){
+        
+    }
+    
+    public Figura (String cor){
+        super ();
+        this.cor = cor;
+    }
+    
+    public String getCor(){
+        return cor;
+    }
+    
+    public void setCor (String cor){
+        this.cor = cor;
+    }
+    
+    @Override
+    public String toString() {
+        return "Figura [cor = "+ cor + "]";
+    }
+    
+    public abstract double area();
+}
